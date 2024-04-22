7fbd95fc43220af9b9a7225614a74c1263826a3b
animesh <phb21013@tezu.ac.in>
2024-04-22 11:19:52 +0530

    mongodb connnection

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// require('dotenv').config() this line makes code unconsistant => line 2 and 5 to 7
+
+import dotenv from "dotenv"
+import connectDB from "./db/dbindex.js"
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
