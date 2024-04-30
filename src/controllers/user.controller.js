867a67c98ba7c71b97772809833c002d35c59a7f
animesh <phb21013@tezu.ac.in>
2024-04-30 23:09:31 +0530

    upload

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,19 @@
 import {asyncHandler} from "../utils/asyncHandelers.js"
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.json({
-        message: "routes is doing file"
-    })
+    //get user details from frontend
+    // validation - not empty
+    //check if user already exists - username/email
+    //check for images and avtar
+    // upload them to cloudinary, avtar
+    // create userObject- create  entry in db
+    // remove password and refresh token field from response
+    // check user creation 
+    // return res
+
+
+   const {fullName, email, username, password} = req.body
+   console.log("email:", email);
 })
 
 export {registerUser}
